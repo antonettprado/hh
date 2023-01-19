2882c90c93b77cadfd072b9be451f11999d4816b
Antonett <presurvivor@gmail.com>
2023-01-18 16:58:29 -0800

    working version

--- NanoAOD_setup/HH_bbWW/src/HH_bbWW_event_selection.py
+++ NanoAOD_setup/HH_bbWW/src/HH_bbWW_event_selection.py
@@ -7,11 +7,6 @@ from HH_bbWW_selection_funcs import *
 import warnings
 warnings.filterwarnings("ignore")
 
-# ROOT.ROOT.EnableImplicitMT()
-# ROOT.gSystem.CompileMacro("/Users/antonett/Documents/hh/NanoAOD_setup/HH_bbWW/src/common.cc", "k0")
-# ROOT.gSystem.Load("/Users/antonett/Documents/hh/NanoAOD_setup/HH_bbWW/src/common_cc.d")
-# ROOT.gInterpreter.Declare('#include "/Users/antonett/Documents/hh/NanoAOD_setup/HH_bbWW/interface/common.h"')
-
 if __name__ == "__main__":
 
     # Parsing arguments
@@ -39,65 +34,48 @@ if __name__ == "__main__":
     for s in input_json_file[args.sample][args.year]:
         df_list.append(ROOT.RDataFrame("Events", s))
 
-    print("\n\n\nRunning HH bbWW event selection for %s sample: %s for year %s"%(args.type, args.sample, args.year))
+    print("\nRunning HH bbWW event selection for %s sample: %s for year %s"%(args.type, args.sample, args.year))
 
     for df in df_list:
-        # Example
-        # df = df.Define("tight_mu", "Muon_pt>25 && abs(Muon_eta)<2.4")
-        # df = df.Filter("Sum(tight_mu)==2")
-        # df = df.Define("Muon_pt_tight", "Muon_pt[tight_mu]")
-        # df.Display("Muon_pt_tight").Print()
-
-        df.Describe()
 
         print("1) Basic Event Selection ---------------------------------")
         df = df.Filter("PV_npvsGood>=1")    # Primary collision vertex
         df = met_filter(df, args.type)
-        print("\t\t\tCheck")
 
         print("2) Electron Selection ------------------------------------")
-        df = df.Define("loose_e", "Electron_pt>7 && Electron_eta<2.5 && Electron_dxy<0.05 && Electron_dz<0.1 && Electron_sip3d < 8 && Electron_lostHits<=1 && Electron_mvaFall17V2noIso_WPL==1")
-        df = df.Define("fakeable_e", "Electron_pt>10 && Electron_eta<2.5 && Electron_dxy<0.05 && Electron_dz<0.1 && Electron_sip3d < 8 && Electron_hoe<0.10 && Electron_eInvMinusPInv>-0.04 && Electron_lostHits==0 && Electron_mvaFall17V2noIso_WP90==1 && Electron_jetRelIso<0.7")
-        df = df.Define("tight_e", "Electron_pt>10 && Electron_eta<2.5 && Electron_dxy<0.05 && Electron_dz<0.1 && Electron_sip3d < 8 && Electron_hoe<0.10 && Electron_eInvMinusPInv>-0.04 && Electron_lostHits==0 && Electron_mvaFall17V2noIso_WPL==1")
-        print("\t\t\tCheck")
-          
+        df = df.Define("loose_e",   "Electron_pt>7  && Electron_eta<2.5 && Electron_dxy<0.05 && Electron_dz<0.1 && Electron_sip3d < 8 &&                                                      Electron_lostHits<=1 && Electron_mvaFall17V2noIso_WPL==1")
+        df = df.Define("fakeable_e","Electron_pt>10 && Electron_eta<2.5 && Electron_dxy<0.05 && Electron_dz<0.1 && Electron_sip3d < 8 && Electron_hoe<0.10 && Electron_eInvMinusPInv>-0.04 && Electron_lostHits==0 && Electron_mvaFall17V2noIso_WP90==1 && Electron_jetRelIso<0.7")
+        df = df.Define("tight_e",   "Electron_pt>10 && Electron_eta<2.5 && Electron_dxy<0.05 && Electron_dz<0.1 && Electron_sip3d < 8 && Electron_hoe<0.10 && Electron_eInvMinusPInv>-0.04 && Electron_lostHits==0 && Electron_mvaFall17V2noIso_WPL==1")
+        # df.Display({"loose_e","fakeable_e","tight_e"},20).Print()
+
         print("3) Muon Selection ----------------------------------------")
         df = df.Define("loose_mu", "Muon_pt > 5 && Muon_eta<2.4 && Muon_dxy<0.05 && Muon_dz<0.1 && Muon_sip3d < 8 && Muon_looseId==1")
         df = df.Define("fakeable_mu", "Muon_pt > 10 && Muon_eta<2.4 && Muon_dxy<0.05 && Muon_dz<0.1 && Muon_sip3d < 8 && Muon_looseId==1 && Muon_jetRelIso<0.8")
         df = df.Define("tight_mu", "Muon_pt > 10 && Muon_eta<2.4 && Muon_dxy<0.05 && Muon_dz<0.1 && Muon_sip3d < 8 && Muon_mediumId==1")
-        df.Display("fakeable_mu").Print()
-        df.Display("loose_mu").Print()
-        df.Display("tight_mu").Print()
-        print("\t\t\tCheck")
-
-        print("4) Tau Selection ----------------------------------------")
-        print("\t\t\tCheck")
 
         print("5) AK4 Jet Selection ------------------------------------")
         df = df.Define("central_ak4","Jet_pt>25 && Jet_eta<2.4")
-        df = df.Filter("central_ak4").Define("VBF_Jet", "Jet_pt>30 && Jet_eta<4.7")
-        print("\t\t\tCheck")
+        df = df.Define("VBF_Jet", "central_ak4 && Jet_pt>30 && Jet_eta<4.7")
 
         print("6) AK8 Jet Selection ------------------------------------")
         df = df.Define("ak8_jet", "FatJet_pt>200 && FatJet_eta<2.4 && FatJet_msoftdrop>30 && FatJet_msoftdrop<210 ")
-        print("\t\t\tCheck")
 
-        
         print("7) Final Event Selection ---------------------------------")
-        # SL
-        df_sl = selection_sl_channel(df, args.year)
-        # DL
-        df_dl = selection_dl_channel(df, args.year)
-        print("\t\t\tCheck")
+        df_sl = df    
+        df_dl = df
+        df_sl = selection_sl_channel(df_sl, args.year)
+        df_sl.Report().Print()
+        
+        # df_dl = selection_dl_channel(df, args.year)
+        # df_dl.Report().Print()
 
-        # df = df.Define("Z", "findZ(GoodLepton, Lepton_pt, Lepton_eta, Lepton_phi, Lepton_mass, Lepton_pdgId)")
 
-        rvec = ROOT.RVec('double')((1, 2, 3))
-        print(rvec) # { 1.0000000, 2.0000000, 3.0000000 }
+        # print("8) Plotting -------------------------------------------------")
+        # h = df_sl.Histo1D("nMuon")
+        # c = ROOT.TCanvas()  
+        # h.Draw()
+        # c.SaveAs("nMuon.png")
+        # print("Saved figure to nMuon.png")
 
-        # jet_eta = ROOT.VecOps.Take()
-        # deltaR = ROOT.VecOps.DeltaR(jet_eta, lep_eta, jet_phi, lep_phi)
-
-        # Yields
 
 
--- NanoAOD_setup/HH_bbWW/src/HH_bbWW_selection_funcs.py
+++ NanoAOD_setup/HH_bbWW/src/HH_bbWW_selection_funcs.py
@@ -1,53 +1,11 @@
-def selection_sl_channel(df, year):
-
-    df = df.Filter("nMuon+nElectron==1", "one_lepton_cut")
-    df = trigger_filter(df, year)
-    df = df.Filter("Electron_pt>30 || Muon_pt>25")
-    df = df.Filter("Electron_eta<2.5 || Muon_eta<2.4")
-    # df = df.[hadronic tau veto]
-    df= df.Filter("nJet>=1 || nFatJet>=1")
-    # df = df.[]
-    df.Report()
-
-    return df
-
-def selection_dl_channel(df, year):
-    
-    df_2l = df.Filter("nMuon+nElectron==2", "two_lepton_cut")
-    df_tri = trigger_filter(df_2l, year)
-    df_pt = df_tri.Filter("Electron_pt>30 || Muon_pt>25")
-    df_eta = df_pt.Filter("Electron_eta<2.5 || Muon_eta<2.4")
-    df_tauv = df_eta    #hadronic tau veto
-    df_jmul = df_tauv.Filter("nJet>=1 || nFatJet>=1")
-    df_btag = df_jmul
-
-    return df_btag
-
-def trigger_filter(df, year):
-
-    year_dict = {'2016': 0, '2017':1, '2018':2}
-    se_trig_dict = {}
-    se_trig_dict['HLT_Ele25_eta2p1_WPTight_Gsf']    = [1, 0, 0]
-    se_trig_dict['HLT_Ele27_WPTight_Gsf']           = [1, 0, 0]
-    se_trig_dict['HLT_Ele27_eta2p1_WPLoose_Gsf']    = [1, 0, 0]
-    se_trig_dict['HLT_Ele32_WPTight_Gsf']           = [0, 1, 1]
-    se_trig_dict['HLT_Ele35_WPTight_Gsf']           = [0, 1, 0]
-    smu_trig_dict = {}
-    smu_trig_dict['HLT_IsoMu22']            = [1, 0, 0]
-    smu_trig_dict['HLT_IsoTkMu22']          = [1, 0, 0]
-    smu_trig_dict['HLT_IsoMu22_eta2p1']     = [1, 0, 0]
-    smu_trig_dict['HLT_IsoTkMu22_eta2p1']   = [1, 0, 0]
-    smu_trig_dict['HLT_IsoMu24']            = [1, 1, 1]
-    smu_trig_dict['HLT_IsoTkMu24']          = [1, 0, 0]
-    smu_trig_dict['HLT_IsoMu27']            = [0, 1, 1]
-    
-    df = select_filter(df, se_trig_dict, year_dict, year)
-
-    return df
+import ROOT
+import collections
 
 def met_filter(df, sample_type):
-    print("FUNCTION: met_filter")
+
     sample_type_dict = {'data': 0, 'mc': 1}
+    sample_type_column = sample_type_dict[sample_type]
+    
     met_filter_dict = {}
     met_filter_dict['Flag_goodVertices']                        = [1, 1]
     met_filter_dict['Flag_globalSuperTightHalo2016Filter']      = [1, 1]
@@ -58,21 +16,87 @@ def met_filter(df, sample_type):
     # met_filter_dict['Flag_ecalBadCalibReducedMINIAODFilter']  = [1, 1]
     met_filter_dict['Flag_eeBadScFilter']                       = [1, 0]
 
-    df = select_filter(df, met_filter_dict, sample_type_dict, sample_type)
+    df = select_filter(df, met_filter_dict, sample_type_column, "met")
 
     return df
 
-def select_filter(df, filter_dict, key_dict, key_value):
-    
-    column = key_dict[key_value]
 
-    selected_triggers = []
+def selection_sl_channel(df, year):
+
+    print("\t SL channel:")
+    df = trigger_filter(df, year, 1)
+    df = df.Filter("Sum(tight_e)>=1 || Sum(tight_mu)>=1",       "sl_filter_1")
+    df.Display({"nElectron","nMuon","tight_e", "tight_mu", "Electron_pt", "Muon_pt"},10).Print()
+    df = df.Filter("Electron_pt[0]>30 || Muon_pt[0]>25",        "sl_filter_2")
+    df = df.Filter("Electron_eta[0]<2.5 || Muon_eta[0]<2.4",    "sl_filter_3")
+    # df_sl = df_sl.[hadronic tau veto]
+    df = df.Filter("nJet>=1 || nFatJet>=1",                     "sl_filter_4")
+
+    return df
+
+def selection_dl_channel(df, year):
+
+    print("\t DL channel:")
+    # df = df.Filter("nMuon+nElectron==2",                "dl_filter_1")
+    # df = trigger_filter(df, year, 2)
+    # df = df.Filter("Electron_pt>30 || Muon_pt>25",      "dl_filter_2")
+    # df = df.Filter("Electron_eta<2.5 || Muon_eta<2.4",  "dl_filter_3")
+    # df = df.Filter("nJet>=1 || nFatJet>=1",             "dl_filter_4")
+
+    return df
+
+def trigger_filter(df, year, lepton_number):
+
+    year_dict = {'2016': 0, '2017':1, '2018':2}
+    year_column = year_dict[year]
+
+    s_e_trig_dict = {}
+    s_e_trig_dict['HLT_Ele25_eta2p1_WPTight_Gsf']    = [1, 0, 0]
+    s_e_trig_dict['HLT_Ele27_WPTight_Gsf']           = [1, 0, 0]
+    s_e_trig_dict['HLT_Ele27_eta2p1_WPLoos_e_Gsf']    = [1, 0, 0]
+    s_e_trig_dict['HLT_Ele32_WPTight_Gsf']           = [0, 1, 1]
+    s_e_trig_dict['HLT_Ele35_WPTight_Gsf']           = [0, 1, 0]
+    
+    s_mu_trig_dict = {}
+    s_mu_trig_dict['HLT_IsoMu22']            = [1, 0, 0]
+    s_mu_trig_dict['HLT_IsoTkMu22']          = [1, 0, 0]
+    s_mu_trig_dict['HLT_IsoMu22_eta2p1']     = [1, 0, 0]
+    s_mu_trig_dict['HLT_IsoTkMu22_eta2p1']   = [1, 0, 0]
+    s_mu_trig_dict['HLT_IsoMu24']            = [1, 1, 1]
+    s_mu_trig_dict['HLT_IsoTkMu24']          = [1, 0, 0]
+    s_mu_trig_dict['HLT_IsoMu27']            = [0, 1, 1]
+
+    d_e_trig_dict = {}
+    d_e_trig_dict['HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL'] =   [0, 1, 1]
+
+    d_mu_trig_dict = {}
+    d_mu_trig_dict['HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8'] =     [0, 1, 1]
+
+    d_e_mu_trig_dict = {}
+    d_e_mu_trig_dict['HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ'] =     [1, 1, 1]
+
+    if lepton_number == 1:
+        single_lepton_triggers_dict = collections.ChainMap(s_e_trig_dict, s_mu_trig_dict)
+        trigger_dict = single_lepton_triggers_dict
+    elif lepton_number == 2:
+        double_lepton_triggers_dict = collections.ChainMap(s_e_trig_dict, s_mu_trig_dict, d_e_trig_dict, d_mu_trig_dict, d_e_mu_trig_dict)
+        trigger_dict = double_lepton_triggers_dict
+        
+    df = select_filter(df, trigger_dict, year_column, "trigger")
+
+    return df
+
+def select_filter(df, filter_dict, column, filter_tagname):
+    
+    selected_filters = []
     for key in filter_dict.keys():
         if filter_dict[key][column] == 1:
-            selected_triggers.append(key)
+            selected_filters.append(key)
 
-    for trigger in selected_triggers:
-        df = df.Filter(trigger)
-        print(trigger)
+    for i in range(len(selected_filters)):
+        trigger = selected_filters[i]
+        trigger_name = filter_tagname + "_filter_" + str(i)
+        df = df.Filter(trigger, trigger_name)
 
     return df
+
