c4a8a5ebf5a1ff7523e1a0552186047350441e7e
Abhisek Datta <abhisek.datta@cern.ch>
2023-01-30 14:17:49 -0800

    ele ID fix

--- NanoAOD_setup/HH_bbWW/python/HH_bbWW_object_selection.py
+++ NanoAOD_setup/HH_bbWW/python/HH_bbWW_object_selection.py
@@ -75,8 +75,8 @@ def electron_selection(electrons, jets, electrons_sel_index, cuts):
 
         # TO DO: use ID according to Prompt-e MVA for fakeable
         id_cut = cuts["id"]
-        if id_cut == "WP_80_WP_L":
-            id_cut = "WP_80"
+        if id_cut == "WP_80_WP_L" or id_cut == "WP_90_WP_L":
+            id_cut = "WP_L"
         if id_cut == "WP_L":
             if not ele.mvaFall17V2noIso_WPL:
                 continue
