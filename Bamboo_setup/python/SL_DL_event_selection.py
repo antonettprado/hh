a2d93b548914038df65a08931438fe569ebf2509
Samuel Crossley <scrossle@lxplus720.cern.ch>
2023-07-24 23:23:34 +0200

    utils symbolic link

--- Bamboo_setup/python/SL_DL_event_selection.py
+++ Bamboo_setup/python/SL_DL_event_selection.py
@@ -4,6 +4,8 @@ from bamboo import treefunctions as op
 
 import object_definition as object_defs
 import event_definition as event_defs
+from utils import variables
+from utils.variables import Variable1D, Variable2D
 
 from base_selection import NanoBaseHHbbWW
 
