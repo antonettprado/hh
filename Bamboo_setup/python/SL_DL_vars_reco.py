6159bc2bfbca20500ff0387df12f1bbb150eb78a
Antonett Prado <presurvivor@gmail.com>
2023-07-31 23:42:59 +0200

    uncommented postprocessing in reco vars

--- Bamboo_setup/python/SL_DL_vars_reco.py
+++ Bamboo_setup/python/SL_DL_vars_reco.py
@@ -395,9 +395,6 @@ class SL_DL_vars_reco(SL_DL_event_selection):
         subcat_names = trijet_pT_rat.subcats
         selections = self.get_selections_subset(subcat_names)
 
-<<<<<<< HEAD
-    def get_all_reco_2D_variables(self) -> list[Variable2D]:
-=======
         jj, b, _, _ = self._get_top_vars_data()
         trijet = jj[0].p4 + jj[1].p4 + b.p4
         data = trijet.Pt() / (jj[0].pt + jj[1].pt + b.pt)
@@ -508,7 +505,6 @@ class SL_DL_vars_reco(SL_DL_event_selection):
         return vars
 
     def get_all_reco_2D_variables(self) -> 'list[Variable2D]':
->>>>>>> cf07ead87e029564fd839ce654c59228a0e02bde
         vars1D = self.get_all_reco_variables()
         vars1D_lookup = { var.name: var for var in vars1D }
         vars2D = [ Variable2D(name) for name in variables.ALL_VARNAMES_2D ]
@@ -567,7 +563,6 @@ class SL_DL_vars_reco(SL_DL_event_selection):
 
         return plots
 
-<<<<<<< HEAD
     def postProcess(self, taskList, config=None, workdir=None, resultsdir=None):
         super(SL_DL_vars_reco, self).postProcess(taskList, config=config, workdir=workdir, resultsdir=resultsdir)
         print("-------------------- Outputtting 2D histograms ---------------------")
@@ -663,118 +658,3 @@ class SL_DL_vars_reco(SL_DL_event_selection):
         output_llr_file = os.path.join(results_path, "corrections_llr.json")
         with open(output_llr_file, "w") as outfile:
             outfile.write(cset.json(exclude_unset=False))
-=======
-    # def postProcess(self, taskList, config=None, workdir=None, resultsdir=None):
-    #     print("----------------------------- In postProces -----------------------------")
-    #     super(SL_DL_vars_reco, self).postProcess(taskList, config=config, workdir=workdir, resultsdir=resultsdir)
-
-    #     # ------------------- Outputtting 2D histograms ---------------------------
-    #     from bamboo.plots import Plot, DerivedPlot
-    #     plotList_2D = [ ap for ap in self.plotList if ( isinstance(ap, Plot) or isinstance(ap, DerivedPlot) ) and len(ap.binnings) == 2 ]
-    #     from bamboo.analysisutils import loadPlotIt
-    #     p_config, samples, plots_2D, systematics, legend = loadPlotIt(config, plotList_2D, eras=self.args.eras[1], workdir=workdir, resultsdir=resultsdir, readCounters=self.readCounters, vetoFileAttributes=self.__class__.CustomSampleAttributes, plotDefaults=self.plotDefaults)
-    #     from plotit.plotit import Stack
-    #     from bamboo.root import gbl
-    #     for plot in plots_2D:
-    #         expStack = Stack(smp.getHist(plot) for smp in samples if smp.cfg.type == "MC")
-    #         cv = gbl.TCanvas(f"c{plot.name}")
-    #         expStack.obj.Draw("COLZ")
-    #         cv.Update()
-    #         import os
-    #         cv.SaveAs(os.path.join(resultsdir, f"{plot.name}.pdf"))
-
-    #     # ---------------------- Reading scalefactors ------------------------------
-    #     import os
-    #     import correctionlib.convert
-    #     import uproot
-    #     import rich
-    #     import ROOT
-    #     import boost_histogram as bh
-
-    #     SIGNAL_SAMPLES = None
-    #     BACKG_SAMPLES = None
-    #     ALL_SIGNAL_SAMPLES = ['bbWW_sl.root', 'bbWW_dl.root', 'bbtautau.root']
-    #     ALL_BACKG_SAMPLES = ['TTbar_sl.root', 'TTbar_dl.root']
-
-    #     def get_files_in_directory():
-            
-    #         signal_files = []
-    #         backg_files = []
-    #         for filename in os.listdir(results_path):
-    #             file_path = os.path.join(results_path,filename)
-    #             if filename in ALL_SIGNAL_SAMPLES:
-    #                 root_file = ROOT.TFile.Open(file_path, 'read')
-    #                 signal_files.append(root_file)
-    #             elif filename in ALL_BACKG_SAMPLES:
-    #                 root_file = ROOT.TFile.Open(file_path, 'read')
-    #                 backg_files.append(root_file)
-    #         return signal_files, backg_files
-
-    #     def output_llr_hist(object_name, xbins, xmin, xmax):
-            
-    #         signal_total_hist = ROOT.TH1F("signal " + object_name, "", xbins, xmin, xmax)
-    #         backg_total_hist  = ROOT.TH1F("backg " + object_name, "", xbins, xmin, xmax)
-
-    #         object_name = "SL_res_2b_x_" + object_name
-
-    #         for sample in SIGNAL_SAMPLES:
-    #             sample_signal = sample.Get(object_name)
-    #             signal_total_hist.Add(sample_signal)
-
-    #         for sample in BACKG_SAMPLES:
-    #             backg_signal = sample.Get(object_name)
-    #             backg_total_hist.Add(backg_signal)
-
-    #         # Normalize signal and background -----------
-    #         signal_total_hist.Scale(1/signal_total_hist.Integral())
-    #         backg_total_hist.Scale(1/backg_total_hist.Integral())
-
-    #         ratio_hist = ROOT.TH1F("object_name_ratio", "", xbins, xmin, xmax)
-    #         ratio_hist = signal_total_hist.Clone()
-    #         ratio_hist.Divide(backg_total_hist)
-
-    #         ratio_hist.Write(object_name)
-
-    #         return root_file
-
-    #     results_path = os.path.join(self.args.output,'results')
-    #     SIGNAL_SAMPLES, BACKG_SAMPLES = get_files_in_directory()
-
-    #     root_file = ROOT.TFile(os.path.join(results_path, "output_file.root"), "RECREATE")
-
-    #     interesting_vars = []
-    #     interesting_vars.append(["bjets_mbb", BJETS_MBB_BINS, BJETS_MBB_MIN, BJETS_MBB_MAX])
-    #     interesting_vars.append(["bjets_dEta", BJETS_DETA_BINS, BJETS_DETA_MIN, BJETS_DETA_MAX])
-    #     interesting_vars.append(["bjets_dPhi", BJETS_DPHI_BINS, BJETS_DPHI_MIN, BJETS_DPHI_MAX])
-    #     interesting_vars.append(["bjets_pT_bb", BJETS_MBB_BINS, BJETS_MBB_MIN, BJETS_MBB_MAX])
-
-    #     for var in interesting_vars:
-    #         output_llr_hist(var[0], var[1], var[2], var[3])
-
-    #     root_file.Close()
-
-    #     #---------------------------------------------------------------------
-
-    #     all_corrections = []
-    #     with uproot.open(os.path.join(results_path, "output_file.root")) as root_file:
-    #         for key in root_file.keys():
-    #             end_index = key.rfind(';')
-    #             hist_name = key[:end_index]
-    #             # ratio_hists[hist_name] = root_file[key] 
-    #             hist = root_file[key]
-
-    #             h = bh.Histogram(hist)
-
-    #             corr = correctionlib.convert.from_histogram(h)
-    #             corr.name = hist_name
-    #             corr.description = f"llr for " + hist_name
-    #             # corr.data.flow = "clamp"
-    #             rich.print(corr)
-    #             all_corrections.append(corr)
-
-    #     cset = correctionlib.schemav2.CorrectionSet(schema_version=2, description=f"Likelihood corrections", corrections=all_corrections) 
-
-    #     output_llr_file = os.path.join(results_path, "corrections_llr.json")
-    #     with open(output_llr_file, "w") as outfile:
-    #         outfile.write(cset.json(exclude_unset=False))
->>>>>>> cf07ead87e029564fd839ce654c59228a0e02bde
