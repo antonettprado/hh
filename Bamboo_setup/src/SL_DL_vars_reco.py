87d5aae83b4b54726d6250d5d9d5b510369936e6
Abhisek Datta <abhisek.datta@cern.ch>
2023-08-12 18:51:55 -0700

    add HT to reco Vars

--- Bamboo_setup/src/SL_DL_vars_reco.py
+++ Bamboo_setup/src/SL_DL_vars_reco.py
@@ -502,13 +502,26 @@ class SL_DL_vars_reco(SL_DL_event_selection):
         data = { "SL_res_2b_x": data, 'SL_res_2b': data, "DL_res_2b": data }
         all_mT.populate(data, selections)
         return all_mT
+    
+    def get_all_jets_HT(self) -> Variable1D:
+        all_jets_HT = Variable1D('all_jets_HT')
+        subcat_names = all_jets_HT.subcats
+        selections = self.get_selections_subset(subcat_names)
+
+        electrons, muons, met, jets = self._get_total_vars_data()
+        total_jet_pt = op.rng_sum(jets, lambda jet: jet.pt)
+        data = total_jet_pt
+        data = { "SL_res_2b_x": data, 'SL_res_2b': data,"DL_res_2b": data }
+        all_jets_HT.populate(data, selections)
+        return all_jets_HT
 
     # Helper function for returning a list of all 'total' variables for iteration
     def get_total_vars(self) -> 'list[Variable1D]':
         vars = [self.get_all_sT(),
                 self.get_all_sT_50_cut(),
                 self.get_all_mInv(),
-                self.get_all_mT()]
+                self.get_all_mT(),
+                self.get_all_jets_HT()]
         return vars
           
     def get_misc_vars(self) -> 'list[Variable1D]':
