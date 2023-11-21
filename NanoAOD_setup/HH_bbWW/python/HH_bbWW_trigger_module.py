1d946a4827c95ab828365f444d5ac8122571faf2
Abhisek Datta <abhisek.datta@cern.ch>
2023-11-21 11:41:42 -0800

    update L1 seed tests

--- NanoAOD_setup/HH_bbWW/python/HH_bbWW_trigger_module.py
+++ NanoAOD_setup/HH_bbWW/python/HH_bbWW_trigger_module.py
@@ -30,6 +30,10 @@ class HH_bbWW_Trigger_Analysis(Module):
         self.n_event_sl_gen_e = 0
         self.n_event_sl_gen_tau = 0
         self.n_event_sl_gen_mu_L1_Mu22 = 0
+
+        self.n_event_sl_eg_L1_SingleIsoEG24er2p1 = 0
+        self.n_event_sl_eg_L1_SingleIsoEG24er2p1_emulated = 0
+
         self.cuts = json.load(open("data/input_HH_bbWW_cuts.json"))
 
     def endJob(self):
@@ -44,6 +48,11 @@ class HH_bbWW_Trigger_Analysis(Module):
         print ("Total nr. of SL Mu Events (1 Gen Mu from W): %d\n"%self.n_event_sl_gen_mu)
         print ("Total nr. of SL Tau Events (1 Gen Tau from W): %d\n"%self.n_event_sl_gen_tau)
         print ("Total nr. of SL Mu Events (1 Gen Mu from W) passing L1 Mu22: %d, Efficiency: %.4f \n"%(self.n_event_sl_gen_mu_L1_Mu22, self.n_event_sl_gen_mu_L1_Mu22/self.n_event_sl_gen_mu))
+        print("")
+        print ("Total nr. of SL EG Events: %d\n"%self.n_event_sl_e)
+        print ("Total nr. of SL EG Events passing L1 SingleIsoEG24er2p1: %d, Efficiency: %.4f \n"%(self.n_event_sl_eg_L1_SingleIsoEG24er2p1, self.n_event_sl_eg_L1_SingleIsoEG24er2p1/self.n_event_sl_e))
+        print ("Total nr. of SL EG Events passing L1 SingleIsoEG24er2p1 Emulated: %d, Efficiency: %.4f \n"%(self.n_event_sl_eg_L1_SingleIsoEG24er2p1_emulated, self.n_event_sl_eg_L1_SingleIsoEG24er2p1_emulated/self.n_event_sl_e))
+        print("")
 
     def analyze(self, event):
 
@@ -181,17 +190,24 @@ class HH_bbWW_Trigger_Analysis(Module):
         if is_sl_mu and (l1.SingleMu22 or l1.Mu6_HTT250er):
             self.n_event_sl_mu_L1_Mu22_OR_Mu6_HT250 += 1
 
-        SingleIsoEG35_emulated = 0
+        SingleIsoEG24er2p1_emulated = 0
         for ele in l1_electrons:
-            if (ele.pt >= 35 and ele.hwIso >= 3):
-                SingleIsoEG35_emulated = 1
+            if (ele.pt >= 24 and abs(ele.eta) <= 2.131 and ele.hwIso >= 3):
+                SingleIsoEG24er2p1_emulated = 1
                 break
+        
+        if is_sl_e:
+            self.n_event_sl_e += 1
+        if is_sl_e and l1.SingleIsoEG24er2p1:
+            self.n_event_sl_eg_L1_SingleIsoEG24er2p1 += 1
+        if is_sl_e and SingleIsoEG24er2p1_emulated:
+            self.n_event_sl_eg_L1_SingleIsoEG24er2p1_emulated += 1
 
         print ("Event: ", event.event)
-        if l1.SingleIsoEG35 != SingleIsoEG35_emulated:
-            print ("Flag: ", l1.SingleIsoEG35, "Emulated: ", SingleIsoEG35_emulated)
+        if l1.SingleIsoEG24er2p1 != SingleIsoEG24er2p1_emulated:
+            print ("Flag: ", l1.SingleIsoEG24er2p1, "Emulated: ", SingleIsoEG24er2p1_emulated)
             for ele in l1_electrons:
-                print ("SingleIsoEG35: ", ele.pt, ele.eta, ele.hwIso)
+                print (ele.pt, ele.eta, ele.hwIso)
             print ("")
         #if l1.LooseIsoEG28er2p1_HTT100er:
         #    for ele in l1_electrons:
