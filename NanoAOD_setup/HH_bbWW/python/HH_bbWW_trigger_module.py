7231c80e3a95e72b02b28e3d449440a366ebd26a
Abhisek Datta <abhisek.datta@cern.ch>
2023-11-21 10:47:12 -0800

    update L1 seed tests

--- NanoAOD_setup/HH_bbWW/python/HH_bbWW_trigger_module.py
+++ NanoAOD_setup/HH_bbWW/python/HH_bbWW_trigger_module.py
@@ -181,14 +181,20 @@ class HH_bbWW_Trigger_Analysis(Module):
         if is_sl_mu and (l1.SingleMu22 or l1.Mu6_HTT250er):
             self.n_event_sl_mu_L1_Mu22_OR_Mu6_HT250 += 1
 
-        if l1.SingleIsoEG35:
+        SingleIsoEG35_emulated = 0
+        for ele in l1_electrons:
+            if (ele.pt >= 35 and ele.hwIso >= 3):
+                SingleIsoEG35_emulated = 1
+
+        if l1.SingleIsoEG35 != SingleIsoEG35_emulated:
+            print ("Flag: ", l1.SingleIsoEG35, "Emulated: ", SingleIsoEG35_emulated)
             for ele in l1_electrons:
                 print ("SingleIsoEG35: ", ele.pt, ele.eta, ele.hwIso)
         print ("")
-        if l1.LooseIsoEG28er2p1_HTT100er:
-            for ele in l1_electrons:
-                print ("LooseIsoEG28er2p1_HTT100er: ", ele.pt, ele.eta, ele.hwIso)
-        print ("\n")
+        #if l1.LooseIsoEG28er2p1_HTT100er:
+        #    for ele in l1_electrons:
+        #        print ("LooseIsoEG28er2p1_HTT100er: ", ele.pt, ele.eta, ele.hwIso)
+        #print ("\n")
 
         if not is_sl and not is_dl:
             return False
