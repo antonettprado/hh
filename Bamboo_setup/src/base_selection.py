de1cc36cfb392a47b630846433b59aaef53e7b2e
Abhisek Datta <abhisek.datta@cern.ch>
2024-01-04 21:27:07 -0800

    bug fix

--- Bamboo_setup/src/base_selection.py
+++ Bamboo_setup/src/base_selection.py
@@ -3,6 +3,7 @@ from bamboo.treedecorators import NanoAODDescription
 from bamboo.analysisutils import makeMultiPrimaryDatasetTriggerSelection
 from bamboo import treefunctions as op
 from bamboo.plots import Plot, CutFlowReport
+from bamboo.plots import EquidistantBinning as EqBin
 
 from itertools import chain
 
@@ -61,7 +62,7 @@ class NanoBaseHHbbWW(NanoAODHistoModule):
             noSel = noSel.refine("Veto super-weighted events in HH", cut=(op.abs(tree.genWeight) < 100))
             self.yields.add(noSel, "Veto super-weighted events in HH")
             # Add neccesary plot for corrected sum of genWeights 
-            self.base_plots.append(Plot.make1D("generated_sum_corrected", op.c_float(0.5), self.noSel, EqBin(1,0.,1.), autoSyst=False))
+            self.base_plots.append(Plot.make1D("generated_sum_corrected", op.c_float(0.5), self._noSel, EqBin(1,0.,1.), autoSyst=False))
         
         self.noSel = noSel
         
