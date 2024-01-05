a1a1b7744ad080b94ddbf1b342cc61fed42c9e97
Antonett Prado <presurvivor@gmail.com>
2024-01-05 22:19:43 +0100

    added final EG seeds; brought back genEventSumWeight for LLRs

--- Bamboo_setup/src/SL_DL_event_selection.py
+++ Bamboo_setup/src/SL_DL_event_selection.py
@@ -121,8 +121,8 @@ class SL_DL_event_selection(NanoBaseHHbbWW):
             raise ValueError("events must be 'all', 'odd', or 'even'")
 
         # Gen the base selection from base_selection, refine it with the relevant cut, and add to the yields table
-        noSelweighted = self.noSel.refine('events cut on noSelweighted', cut=cut)        
-        yields.add(noSelweighted, "Sample Sum of Weights") # This changes the yields in the list, even though we don't return it!
+        noSel = self.noSel.refine('genEventSumWeight', cut=cut)        
+        yields.add(noSel, "Sample Sum of Weights") # This changes the yields in the list, even though we don't return it!
 
         # Refine the working selection (baseSel) with the parity cut
         baseSel = baseSel.refine(events, cut=cut)
