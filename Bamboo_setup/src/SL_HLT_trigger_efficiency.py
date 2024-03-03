83dda731e02ae119e0563459357f679db01d30cd
Abhisek Datta <abhisek.datta@cern.ch>
2024-03-02 22:20:08 -0800

    plot fix

--- Bamboo_setup/src/SL_HLT_trigger_efficiency.py
+++ Bamboo_setup/src/SL_HLT_trigger_efficiency.py
@@ -382,7 +382,7 @@ class SL_HLT_trigger_efficiency(SL_L1_trigger_efficiency):
                     Plot.make1D(sel_name + "_eta", lep[0].eta, sel, EqBin(50, -4, 4)),
                     Plot.make1D(sel_name + "_HT", self.HT, sel, VariableBinning([0,100,120,140,160,180,200,220,240,260,280,300,350,400,450,500,600,700,800,1000])),
                     Plot.make1D(sel_name + "_npv", tree.PV.npvs, sel, VariableBinning([0,20,25,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58,60,65,70,100])),
-                    Plot.make1D(sel_name + "npv_good", tree.PV.npvsGood, sel, VariableBinning([0,20,25,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58,60,65,70,100]))
+                    Plot.make1D(sel_name + "_npv_good", tree.PV.npvsGood, sel, VariableBinning([0,20,25,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58,60,65,70,100]))
                 ])
         
         return plots
