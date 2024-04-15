6a090bba53b4c0a428afdde61d6ad806076a5016
Abhisek Datta <abhisek.datta@cern.ch>
2024-04-15 15:18:07 -0700

    NN updates

--- Bamboo_setup/src/post_processing/NN/bbWW_NN.py
+++ Bamboo_setup/src/post_processing/NN/bbWW_NN.py
@@ -282,9 +282,10 @@ if __name__ == "__main__":
 
     ## =========================== Plot Signal and Background Output score distributions =============================
     fig, ax = plt.subplots()
+    output_df_signal
     ax.set_xlim(0, 1)
-    ax.hist(output_df['isSignal' == 1.0]['Prediction Score'], bins=100, color='blue', label='Signal', density=True)
-    ax.hist(output_df['isSignal' == 0.0]['Prediction Score'], bins=100, color='red', label='Background', density=True)
+    ax.hist(output_df.loc['isSignal' == 1.0, 'Prediction Score'], bins=100, color='blue', label='Signal', density=True)
+    ax.hist(output_df.loc['isSignal' == 0.0, 'Prediction Score'], bins=100, color='red', label='Background', density=True)
     ax.legend()
     ax.set_xlabel('DNN score')
     ax.set_ylabel('Normalized number of events')
