84f9403b6395f7b32b7f6e5a77a8670220dd4fd8
Abhisek Datta <abhisek.datta@cern.ch>
2024-04-15 15:14:07 -0700

    NN updates

--- Bamboo_setup/src/post_processing/NN/bbWW_NN.py
+++ Bamboo_setup/src/post_processing/NN/bbWW_NN.py
@@ -282,7 +282,7 @@ if __name__ == "__main__":
 
     ## =========================== Plot Signal and Background Output score distributions =============================
     fig, ax = plt.subplots()
-    ax.xlim(0, 1)
+    ax.set_xlim(0, 1)
     ax.hist(output_df['isSignal' == 1.0]['Prediction Score'], bins=100, color='blue', label='Signal', density=True)
     ax.hist(output_df['isSignal' == 0.0]['Prediction Score'], bins=100, color='red', label='Background', density=True)
     ax.legend()
