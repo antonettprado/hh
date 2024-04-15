e2fc91ec6b0a3803f9c614a780c92077c3fd81fa
Abhisek Datta <abhisek.datta@cern.ch>
2024-04-15 15:59:58 -0700

    NN updates

--- Bamboo_setup/src/post_processing/NN/bbWW_NN.py
+++ Bamboo_setup/src/post_processing/NN/bbWW_NN.py
@@ -165,8 +165,8 @@ if __name__ == "__main__":
     model = Model(inputs=inputs, outputs=outputs)
     model.compile(
         optimizer = "adam", # Optimizer
-        #loss      = "binary_crossentropy", # Loss function to minimize
-        loss      = CategoricalCrossentropy(), # Loss function to minimize
+        loss      = "binary_crossentropy", # Loss function to minimize fpr binary ANN
+        #loss      = CategoricalCrossentropy(), # Loss function to minimize for multiclass ANN
         #metrics   = ["accuracy"]
         metrics   = [
             BinaryAccuracy(),
@@ -283,8 +283,8 @@ if __name__ == "__main__":
     ## =========================== Plot Signal and Background Output score distributions =============================
     fig, ax = plt.subplots()
     ax.set_xlim(0, 1)
-    ax.hist(output_df.loc[output_df['isSignal'] == 1.0, 'Prediction Score']/true_signal, bins=100, color='blue', label='Signal', histtype='step')
-    ax.hist(output_df.loc[output_df['isSignal'] == 0.0, 'Prediction Score']/true_background, bins=100, color='red', label='Background', histtype='step')
+    ax.hist(output_df.loc[output_df['isSignal'] == 1.0, 'Prediction Score'], bins=100, color='blue', label='Signal', histtype='step', density=True)
+    ax.hist(output_df.loc[output_df['isSignal'] == 0.0, 'Prediction Score'], bins=100, color='red', label='Background', histtype='step', density=True)
     ax.legend()
     ax.set_xlabel('DNN score')
     ax.set_ylabel('Normalized number of events')
