e8373a1adb145c8c410763e318359af4738bf835
Abhisek Datta <abhisek.datta@cern.ch>
2024-04-24 06:38:37 -0700

    llr update

--- Bamboo_setup/src/SL_DL_likelihood_ratio.py
+++ Bamboo_setup/src/SL_DL_likelihood_ratio.py
@@ -246,8 +246,7 @@ class SL_DL_likelihood_ratio(SL_DL_vars_reco):
         lrs_for_vars_custom_combos = self.get_lrs_for_vars_custom_combos()
         all_lrs = lrs_for_vars_1D + lrs_for_vars_custom_combos
 
-        #hists_1D = [Plot.make1D(subcat_lr.ref, subcat_lr.data, subcat_lr.selection, lr.eqbin) for lr in all_lrs for subcat_lr in lr if subcat_lr.subcat == "SL_res_2b_x"]
-        hists_1D = [Plot.make1D(subcat_lr.ref, subcat_lr.data, subcat_lr.selection, lr.eqbin) for lr in all_lrs for subcat_lr in lr]
+        hists_1D = [Plot.make1D(subcat_lr.ref, subcat_lr.data, subcat_lr.selection, lr.eqbin) for lr in all_lrs for subcat_lr in lr if subcat_lr.subcat == "SL_res_2b_x"]
         plots.extend(hists_1D)
 
         # plots = self.test_skim_refined(all_lrs, SL_res_2b_x, plots)
