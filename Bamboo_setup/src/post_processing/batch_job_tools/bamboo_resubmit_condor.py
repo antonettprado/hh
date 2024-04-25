951f5dcdf0af3d7c01520d728e82e11fbd28e2e2
Abhisek Datta <abhisek.datta@cern.ch>
2024-04-24 22:31:07 -0700

    bamboo resubmit script

--- Bamboo_setup/src/post_processing/batch_job_tools/bamboo_resubmit_condor.py
+++ Bamboo_setup/src/post_processing/batch_job_tools/bamboo_resubmit_condor.py
@@ -7,11 +7,17 @@ if __name__ == '__main__':
     parser.add_argument("-i", "--input_dir", action="store", help="directory to resubmit jobs for")
     args = parser.parse_args()
 
+    job_resubmit = []
     jobs = glob.glob("%s/batch/output/*"%args.input_dir)
     for job in jobs:
         root_file = glob.glob("%s/*.root"%job)[0]
         root_file_size = os.path.getsize(root_file)
         job_id = job.split("/")[-1]
         if root_file_size == 0:
-            print (job_id)
+            print ("Resubmitting Job %s"%job_id)
+            job_resubmit.append(job_id)
+    
+    job_resubmit = ','.join(job_resubmit)
+
+    os.system("bambooHTCondorResubmit --ids=%s %s/batch/input/condor.cmd"%(job_resubmit, args.input_dir))
 
