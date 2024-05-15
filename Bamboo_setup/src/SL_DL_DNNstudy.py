2ee786155fdb068c5494cef4ecc5eaa58c55b7f5
Antonett Prado <presurvivor@gmail.com>
2024-05-15 21:08:54 +0200

    minor edit

--- Bamboo_setup/src/SL_DL_DNNstudy.py
+++ Bamboo_setup/src/SL_DL_DNNstudy.py
@@ -7,19 +7,19 @@ import bamboo.treefunctions as op
 
 from SL_DL_vars_gen import SL_DL_vars_gen
 from SL_DL_vars_reco import SL_DL_vars_reco
-from SL_DL_NN_v2 import SL_DL_NN_v2
+from SL_DL_NN import SL_DL_NN
 import utils.variable_definition as var_defs
 from pathlib import Path
 import os
 
-class SL_DL_DNNstudy_v2(NanoAODHistoModule):
+class SL_DL_DNNstudy(NanoAODHistoModule):
     def __init__(self, args):
-        super(SL_DL_DNNstudy_v2, self).__init__(args)
+        super(SL_DL_DNNstudy, self).__init__(args)
         self.event_nr_sel = "odd"
         self.output_llr = False
 
     def addArgs(self, parser):
-        super(SL_DL_DNNstudy_v2, self).addArgs(parser)
+        super(SL_DL_DNNstudy, self).addArgs(parser)
         parser.add_argument("-cw", "--corr_workdir", action='store', help='The work directory where the correction file is')
         parser.add_argument("-nn", action='store', dest = "NNdir", help='Input NN model directory')
 
@@ -69,7 +69,7 @@ class SL_DL_DNNstudy_v2(NanoAODHistoModule):
             varReaders = []
             return NanoAODDescription(groups=groups, collections=collections, systVariations=varReaders)
 
-        tree, _noSel, backend, lumiArgs = super(SL_DL_DNNstudy_v2, self).prepareTree(tree=tree,
+        tree, _noSel, backend, lumiArgs = super(SL_DL_DNNstudy, self).prepareTree(tree=tree,
                                                                                  sample=sample,
                                                                                  sampleCfg=sampleCfg,
                                                                                  description=getCUSTOMNanoAODDescription(),
@@ -141,7 +141,7 @@ class SL_DL_DNNstudy_v2(NanoAODHistoModule):
         return tree, baseSel, backend, lumiArgs
 
     def readCounters(self, resultsFile):
-        counters = super(SL_DL_DNNstudy_v2, self).readCounters(resultsFile)
+        counters = super(SL_DL_DNNstudy, self).readCounters(resultsFile)
         # Corrections to the generated sum "
         if resultsFile.GetListOfKeys().FindObject('generated_sum_corrected'):
             sample = os.path.basename(resultsFile.GetName())
@@ -164,7 +164,7 @@ class SL_DL_DNNstudy_v2(NanoAODHistoModule):
 
         sel_name = "SL_res_2b_x"
 
-        dnn_score = SL_DL_NN_v2.get_dnn_score(self.args.NNdir, objects)
+        dnn_score = SL_DL_NN.get_dnn_score(self.args.NNdir, objects)
         dnn_score = dnn_score[sel_name]
         plots.append(Plot.make1D(dnn_score.ref, dnn_score.data, dnn_score.selection, dnn_score.eqbin, xTitle=dnn_score.full_title))
 
@@ -186,7 +186,7 @@ class SL_DL_DNNstudy_v2(NanoAODHistoModule):
         return plots
 
     def postProcess(self, taskList, config=None, workdir=None, resultsdir=None):
-        super(SL_DL_DNNstudy_v2, self).postProcess(taskList, config=config, workdir=workdir, resultsdir=resultsdir)
+        super(SL_DL_DNNstudy, self).postProcess(taskList, config=config, workdir=workdir, resultsdir=resultsdir)
 
         from post_processing.sig_bkg_shape_comp.compare_subcategories import main as compare_subcategories
         compare_subcategories(workdir, shape_only=True)
