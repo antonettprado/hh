643e670a2ec19c767d53381112605fde66eb2b74
Abhisek Datta <abhisek.datta@cern.ch>
2024-05-22 14:53:26 -0700

    update get correction

--- Bamboo_setup/src/SL_DL_Study_SystUnc.py
+++ Bamboo_setup/src/SL_DL_Study_SystUnc.py
@@ -110,17 +110,18 @@ class SL_DL_Study_SystUnc(NanoAODHistoModule):
             self.yields.add(_noSel_genWeight, "_noSel_genWeight cut")
 
             noSel = _noSel_genWeight
-
+            self.gen_objects = SL_DL_vars_gen.get_gen_objects(tree)
+            
             # ------------- CHECK CHECK CHECK CHECK ------------------------------------
             # Determine weight scale factor depending on gen level infor ttpair_pt
             if sample in ['TTbar_sl', 'TTbar_dl']:  
                 if self.args.ratio != '1p00pt':
-                    gen_objects = SL_DL_vars_gen.get_gen_objects(tree)
+                    
                     input_workdir = Path(self.args.ttpair_corr_workdir)
                     corr_file = input_workdir / 'results' / 'ttpair_pt_scaling.json'
                     weight_sf = get_correction(corr_file.resolve(), self.args.ratio, params={"xaxis": lambda ttpair_pt: ttpair_pt}, defineOnFirstUse=True, sel=noSel)
 
-                    _, DNNstudy_objs = SL_DL_vars_gen.for_DNN_study(gen_objects)
+                    _, DNNstudy_objs = SL_DL_vars_gen.for_DNN_study(self.gen_objects)
                     ttpair_pt = DNNstudy_objs['ttpair_pt']
                     noSel = noSel.refine('weight_scale_factors', weight=weight_sf(ttpair_pt))
             # --------------------------------------------------------------------------
