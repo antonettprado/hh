9f3d2b1d59456e42bd530089b0f3c17f8081ebfa
Abhisek Datta <abhisek.datta@cern.ch>
2024-09-11 22:11:23 -0700

    minor fix

--- Bamboo_setup/src/post_processing/NN/bbWW_NN_class.py
+++ Bamboo_setup/src/post_processing/NN/bbWW_NN_class.py
@@ -812,6 +812,7 @@ def main(workdir: str, test_models_file: str, sel_name: str, mode:str, total_inp
     set_globals(workdir, sel_name, nnoutdir_name, postfix)
     models_summary_name = 'models_performance.csv'
     if mode == 'ca':
+        global N_MAX_TRAINING
         N_MAX_TRAINING *= (n_splits/(n_splits-1))
     DNN_models_params = get_test_models(test_models_file)
     total_df = load_data(sel_name, total_inputs)
