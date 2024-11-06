031fb34eada8f53e89711c3e18e21048e3ff5a7e
Antonett Prado <presurvivor@gmail.com>
2024-11-06 02:04:58 +0100

    udpated References to new root-file-naming convention

--- Bamboo_setup/src/post_processing/References.py
+++ Bamboo_setup/src/post_processing/References.py
@@ -1,8 +1,11 @@
 import ROOT
+from pathlib import Path
 
 #Sort list by length and in descending order (most specific ones first)
 SELECTIONS = ['_noSel', 'noSel', 'baseSel', 'SL_res_1b', 'SL_res_2b', 'SL_res_2b_x', 'SL_boosted', 'DL_res_1b', 'DL_res_2b', 'DL_boosted', 'Total']
 SELECTIONS.sort(key=len, reverse=True)
+
+ERAS = ['2022', '2022EE', '2023', '2023BPix']
 
 PROCESSES_FILES = dict(
     HH_bbWW=['bbWW_sl', 'bbWW_dl'],
@@ -21,6 +24,26 @@ PROCESSES_FILES = dict(
     #Others=[]
     #Fakes=[]
     )
+
+# Function for new convention of sample naming (E.g. bbWW_sl_2022, bbWW_sl_2022EE)
+def _find_root_files(resultsdir: Path) -> list[Path]:
+    return [file for file in resultsdir.iterdir() if file.suffix=='.root' and '__skeleton__' not in file.name]
+
+def _find_processes(resultsdir: Path) -> list[str]:
+    present_files = _find_root_files(resultsdir)
+    present_process_files = sorted(list(set([f.stem.rsplit('_', 1)[0] for f in present_files])))
+    valid_processes = [proc for proc, proc_files in PROCESSES_FILES.items() if any(pf in present_process_files for pf in proc_files)]
+    return valid_processes
+
+def _find_eras(resultsdir: Path) -> list[str]:
+    present_files = _find_root_files(resultsdir)
+    present_eras = sorted(list(set([f.stem.rsplit('_', 1)[1] for f in present_files])))
+    valid_eras = [era for era in ERAS if era in present_eras]
+    return valid_eras
+
+# Color scheme for plotting processes -----------------
+
+_get_color_for = lambda cat, ROOT_b : CLASS_COLOR_MAP[cat][1] if ROOT_b else CLASS_COLOR_MAP[cat][0]
 
 CLASS_COLOR_MAP = dict(
     HH_bbWW=['blue', ROOT.kBlue],
@@ -43,11 +66,4 @@ CLASS_COLOR_MAP = dict(
     Top=['pink', ROOT.kPink],
     AllBackgrounds=['black', ROOT.kBlack]
     )
-
-
-_find_processes = lambda resultsdir: sorted(list(set([proc for proc, files in PROCESSES_FILES.items() for f in resultsdir.iterdir() if f.stem in files ])))
-
-_find_root_files = lambda resultsdir: [file for file in resultsdir.iterdir() if file.suffix=='.root' and '__skeleton__' not in file.name]
-
-_get_color_for = lambda cat, ROOT_b : CLASS_COLOR_MAP[cat][1] if ROOT_b else CLASS_COLOR_MAP[cat][0]
     
