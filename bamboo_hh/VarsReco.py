5c9cfdc356d5aa1c79e34eac774e755a4325f798
Abhisek Datta <abhisek.datta@cern.ch>
2025-03-04 19:51:37 -0800

    remove era

--- bamboo_hh/VarsReco.py
+++ bamboo_hh/VarsReco.py
@@ -111,7 +111,7 @@ class VarsReco(NanoBaseHHbbWW):
         return selections
 
     @staticmethod
-    def get_skim(vars1d: list[Variable], selection, subcat: str, era):
+    def get_skim(vars1d: list[Variable], selection, subcat: str):
         skim_data = {
             "event": None,
             "run": None,
@@ -119,7 +119,6 @@ class VarsReco(NanoBaseHHbbWW):
             "genWeight": None,
             "bunchCrossing": None,
             "genTtbarId": None
-            #"era": era
             }
         subcat_vars: list[Variable] = [ var[subcat] for var in vars1d if subcat in var.subcats ]
         skim_data.update({v.name: v.data for v in subcat_vars})
@@ -179,7 +178,7 @@ class VarsReco(NanoBaseHHbbWW):
             # Verify that the skim selections in the list self.args.skim_selections are in selections
             assert all(skim_sel in selections for skim_sel in self.args.skim_selections), f"Skim selections {self.args.skim_selections} not in selections"
             for skim_selection in self.args.skim_selections:
-                plots.append(VarsReco.get_skim(vars1d, selections[skim_selection], skim_selection, self.era))
+                plots.append(VarsReco.get_skim(vars1d, selections[skim_selection], skim_selection))
 
         return plots
 
