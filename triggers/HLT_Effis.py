e60e8db4aa9ac8728f22bdf394e484ccad406444
Antonett Prado <presurvivor@gmail.com>
2025-09-17 23:32:54 +0200

    working HLT effi script!

--- triggers/HLT_Effis.py
+++ triggers/HLT_Effis.py
@@ -9,14 +9,15 @@ from bamboo.plots import VariableBinning
 # from bamboo_hh.EventSelection import EventSelection
 # import bamboo_hh.definitions.event_definition as event_defs
 from bamboo_hh.BaseSelection import NanoBaseHHbbWW, get_nano_version
-from bamboo_hh.core.getters import get_objects, get_event_selections
+from bamboo_hh.core.getters import get_objects
+from bamboo_hh.core import selections
 from bamboo_hh.interface.selection_bundles import SelectionBundle, SelectionBundleContainer
 
 from pathlib import Path
+import os
 import numpy as np
 import yaml
 import pandas as pd
-from typing import Dict
 
 class HLT_Effis(NanoAODHistoModule):
 
@@ -99,11 +100,8 @@ class HLT_Effis(NanoAODHistoModule):
         paths_EG['Trigger'] = paths_EG.index.str.replace("HLT_", "") 
         return paths_Mu, paths_EG
         
-
-    def get_reference_flags(self, lepton_sel_name, HLT) -> Dict[str, bool]:
-
+    def get_reference_flags(self, lepton_sel_name, HLT) -> dict[str, bool]:
         ref_flags = dict()
-
         # Only using 2023 HLT paths
         if lepton_sel_name == "SL_mu":      
             ref_flags['IsoMu24'] = HLT.IsoMu24
@@ -113,9 +111,7 @@ class HLT_Effis(NanoAODHistoModule):
             # ref_flags['Ele28_eta2p1_WPTight_Gsf_HT150'] = HLT.Ele28_eta2p1_WPTight_Gsf_HT150
             ref_flags['Ele15_IsoVVVL_PFHT450'] = HLT.Ele15_IsoVVVL_PFHT450
         ref_flags['PFHT280_QuadPFJet30_PNet2BTagMean0p55'] = HLT.PFHT280_QuadPFJet30_PNet2BTagMean0p55
-
         ref_flags['All'] = op.OR(*[flag for name, flag in ref_flags.items()])
-
         return ref_flags
 
     def definePlots(self, tree, baseSel, sample=None, sampleCfg=None):
@@ -126,7 +122,7 @@ class HLT_Effis(NanoAODHistoModule):
         plots.extend(self.base_plots)
 
         objects: dict = get_objects(tree, self.era, get_nano_version(sampleCfg), lep_pt_from_L1_or_HLT=self.args.lep_pt)
-        selections: dict = get_event_selections(objects, tree.HLT, baseSel, self.is_MC, self.era, sample)
+        # selections: dict = get_event_selections(objects, tree.HLT, baseSel, self.is_MC, self.era, sample)
 
         # # ===============================================================================
         # # ========================== Plots & Yields======================================
@@ -139,7 +135,9 @@ class HLT_Effis(NanoAODHistoModule):
         taus = objects["taus"]
         cleaned_ak4_jets = objects["ak4_jets"]
         cleaned_ak4_btags = objects["ak4_btags"]
+        cleaned_ak4_loose_btags = objects["ak4_loose_btags"]
         cleaned_ak8_btags = objects["ak8_btags"]
+        
         HT = op.rng_sum(cleaned_ak4_jets, lambda jet: jet.pt)
 
         paths_Mu, paths_EG = self.get_paths_to_process()
@@ -151,22 +149,22 @@ class HLT_Effis(NanoAODHistoModule):
         yields.add(self.noSel, 'noSel')
         yields.add(baseSel, 'baseSel')
 
-        mllSel = baseSel.refine("mllSel", cut=[selections.mllSel(loose_electrons, loose_muons)])
+        mllSel = baseSel.refine("mllSel", cut=[selections.mll_selection(loose_electrons, loose_muons)])
         yields.add(mllSel, "baseSel_mllSel")
 
         if self.args.jet_selection is not False:
             print('custom jet selection!')
             print(self.args.jet_selection)
             sl_res_jet_selection_custom = {
-                '3j1b': event_defs.sl_resolved_3j_1b_selection,
-                '3j2b': event_defs.sl_resolved_3j_2b_selection,
-                '4j1b': event_defs.sl_resolved_4j_1b_selection,
-                '4j2b': event_defs.sl_resolved_4j_2b_selection
+                '3j1b': selections.sl_resolved_3j_1b_selection,
+                '3j2b': selections.sl_resolved_3j_2b_selection,
+                '4j1b': selections.sl_resolved_4j_1b_selection,
+                '4j2b': selections.sl_resolved_4j_2b_selection
             }
             sl_res_jet_selection = sl_res_jet_selection_custom.get(self.args.jet_selection)
         else:
             print('default jet selection!')
-            sl_res_jet_selection = event_defs.sl_resolved_jet_selection
+            sl_res_jet_selection = selections.sl_resolved_jet_selection
             
         # =================================================================
         # Muon paths dataframe ============================================
@@ -182,15 +180,15 @@ class HLT_Effis(NanoAODHistoModule):
                 op.rng_len(taus) == 0,
                 tight_muons[0].pt > mu_pt_cut)])
             SL_mu = SL_mu_only.refine("SL muon selection", cut=[op.OR(
-                sl_res_jet_selection(cleaned_ak4_jets, cleaned_ak4_btags, cleaned_ak8_btags),
-                event_defs.sl_boosted_jet_selection(cleaned_ak4_jets, cleaned_ak4_btags, cleaned_ak8_btags))])
+                sl_res_jet_selection(cleaned_ak4_jets, cleaned_ak4_btags, cleaned_ak4_loose_btags, cleaned_ak8_btags),
+                selections.sl_boosted_jet_selection(cleaned_ak4_jets, cleaned_ak4_btags, cleaned_ak4_loose_btags, cleaned_ak8_btags))])
             
             # We want combined efficiency L1 + HLT =========================
             yields.add(SL_mu, "SL_mu")
             selections_to_plot["SL_mu"] = SL_mu
 
             # Retrieve reference flags =====================================
-            HLT_Mu_ref_flags = self.get_reference_flags("SL_mu")
+            HLT_Mu_ref_flags = self.get_reference_flags("SL_mu", tree.HLT)
             for HLT_flag_name, HLT_flag in HLT_Mu_ref_flags.items():
                 sel_flag_name = 'SL_mu_HLT_' + HLT_flag_name
                 sel_flag = SL_mu.refine(sel_flag_name, cut=[HLT_flag])
@@ -231,15 +229,15 @@ class HLT_Effis(NanoAODHistoModule):
                 op.rng_len(taus) == 0,
                 tight_electrons[0].pt > e_pt_cut)])
             SL_e = SL_e_only.refine("SL electron selection", cut=[op.OR(
-                sl_res_jet_selection(cleaned_ak4_jets, cleaned_ak4_btags, cleaned_ak8_btags),
-                event_defs.sl_boosted_jet_selection(cleaned_ak4_jets, cleaned_ak4_btags, cleaned_ak8_btags))])
+                sl_res_jet_selection(cleaned_ak4_jets, cleaned_ak4_btags, cleaned_ak4_loose_btags, cleaned_ak8_btags),
+                selections.sl_boosted_jet_selection(cleaned_ak4_jets, cleaned_ak4_btags, cleaned_ak4_loose_btags,cleaned_ak8_btags))])
 
             # We want combined efficiency L1 + HLT =========================
             yields.add(SL_e, "SL_e")
             selections_to_plot["SL_e"] = SL_e
 
             # Retrieve reference flags =====================================
-            HLT_EG_ref_flags = self.get_reference_flags("SL_e")
+            HLT_EG_ref_flags = self.get_reference_flags("SL_e", tree.HLT)
             for HLT_flag_name, HLT_flag in HLT_EG_ref_flags.items():
                 sel_flag_name = 'SL_e_HLT_' + HLT_flag_name
                 sel_flag = SL_e.refine(sel_flag_name, cut=[HLT_flag])
@@ -288,6 +286,71 @@ class HLT_Effis(NanoAODHistoModule):
 
         return plots
 
+    def postProcess(self, taskList, config=None, workdir=None, resultsdir=None):
+        super(HLT_Effis, self).postProcess(taskList, config=config, workdir=workdir, resultsdir=resultsdir)
+
+        import os
+        yields_file = os.path.join(workdir, 'yields_' + str(self.era) + '.tex')
+        with open(yields_file, 'r') as file: lines = file.readlines()
+
+        # Extracting relevant info from yields table into table_data
+        table_data = []
+        for line_number, line in enumerate(lines, 1):
+            if line_number >= 26 and line_number <= (len(lines)-2):
+                line_data = line.strip().split('&')  # Modify this according to table structure
+                sel_name = line_data[0].strip()
+                if "---" in line:
+                    eff = float("nan")
+                    eff_error = float("nan")
+                else:
+                    eff = line_data[1].split(r'\pm')[0]
+                    eff = eff.strip()[1:]
+                    eff_error = line_data[1].split(r'\pm')[1]
+                    eff_error = eff_error.strip()[:-4]
+                table_data.append([sel_name, eff, eff_error])
+
+        # Making pandas dataframe from table
+        df = pd.DataFrame(table_data)
+        df.columns = ['Selection', 'Yield', 'Yield_Error']
+        df = df.set_index('Selection')
+        df.index.names = [None]
+        df['Yield'] = df['Yield'].astype(float)
+        df['Yield_Error'] = df['Yield_Error'].astype(float)
+
+        # Adding efficiency column to pandas df: Effi will depend on type of selection name
+        nom_mu_sel = df.loc['SL_mu', 'Yield'] if 'SL_mu' in df.index else 0
+        nom_e_sel = df.loc['SL_e', 'Yield'] if 'SL_e' in df.index else 0
+        nom_noSel = df.loc['noSel', 'Yield'] if 'noSel' in df.index else 0
+        nom_baseSel = df.loc['baseSel', 'Yield'] if 'baseSel' in df.index else 0
+        nom_genMuonSel = df.loc['genMuonSel', 'Yield'] if 'genMuonSel' in df.index else 0
+        nom_genMuonsFromWSel = df.loc['genMuonsFromWSel', 'Yield'] if 'genMuonsFromWSel' in df.index else 0
+
+        def set_value_based_on_index(index):
+            sel_yield = df.loc[index, 'Yield'] 
+            if 'SL_mu' in index:
+                return sel_yield / nom_mu_sel  # Calculate efficiency based on condition
+            elif 'SL_e' in index:
+                return sel_yield / nom_e_sel  # Calculate efficiency based on condition
+            elif 'noSel' in index:
+                return sel_yield / nom_noSel
+            elif 'baseSel' in index:
+                return sel_yield / nom_baseSel
+            elif 'genMuonSel' in index:
+                return sel_yield / nom_genMuonSel
+            elif 'genMuonsFromWSel' in index:
+                return sel_yield / nom_genMuonsFromWSel
+            else:
+                return None  # Set default value if no condition is met
+
+        # Apply the function to create a new column based on the index
+        df['Efficiency'] = df.index.to_series().apply(set_value_based_on_index)
+
+        # Rounding every value in table to 3 digits
+        df['Efficiency'] = df['Efficiency'].apply(lambda x: round(x, 3) if not pd.isnull(x) else x)
+        
+        # Converting table to csv and Printing
+        df.to_csv(os.path.join(self.args.output, 'Efficiencies.csv'), index=True)
+        print(df)
 
     '''
     bambooRun -m triggers/HLT_Effis.py bamboo_hh/config/2024_trigger_dev.yml -o /eos/user/a/anunezde/Z_OUTPUT_eos/HLT_Effis
